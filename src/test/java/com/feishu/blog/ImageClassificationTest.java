d06e5e81dbadb12c62036a3eca7854daee925960
wlupus <wlupus@WlupusMac.local>
2025-05-27 16:04:11 +0800

    remove local test cases
    merge master brach (keeping commit "remove local test cases" with
    conflicts on ImageClassificationTest.java)

--- src/test/java/com/feishu/blog/ImageClassificationTest.java
+++ src/test/java/com/feishu/blog/ImageClassificationTest.java
@@ -20,30 +20,30 @@ public class ImageClassificationTest {
     @Resource
     private CozeImageService cozeImageService;
 
-//    @Test
-//    public void test() throws IOException {
-////         MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/Cat_August_2010-3.jpg");
-////        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/dog.jpeg");
-////        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/24.png");
-////        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/25.png");
-////        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/illust_64841566_20211014_160931.jpg");
-////        byte[] imageData = file.getBytes();
-//        try {
-//            var path = "/Users/wlupus/Downloads/25.png";
-//            ClassificationDTO response = localImageService.classifyImageWithPath(path);
-//            log.debug("{}", response);
-//        } catch (Exception e) {
-//        }
-//    }
-//
-//    @Test
-//    public void testCoze() throws IOException {
-////        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/dog.jpeg");
-////        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/24.png");
-////        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/illust_64841566_20211014_160931.jpg");
-////        byte[] imageData = file.getBytes();
-//        var path = "/Users/wlupus/Downloads/25.png";
-//        ClassificationDTO response = cozeImageService.processImageComplianceWithPath(path);
-//        log.debug("{}", response);
-//    }
+    // @Test
+    public void test() throws IOException {
+//         MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/Cat_August_2010-3.jpg");
+//        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/dog.jpeg");
+//        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/24.png");
+//        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/25.png");
+//        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/illust_64841566_20211014_160931.jpg");
+//        byte[] imageData = file.getBytes();
+        try {
+            var path = "/Users/wlupus/Downloads/25.png";
+            ClassificationDTO response = localImageService.classifyImageWithPath(path);
+            log.debug("{}", response);
+        } catch (Exception e) {
+        }
+    }
+
+    // @Test
+    public void testCoze() throws IOException {
+//        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/dog.jpeg");
+//        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/24.png");
+//        MultipartFile file = fileToMultipartFile("/Users/wlupus/Downloads/illust_64841566_20211014_160931.jpg");
+//        byte[] imageData = file.getBytes();
+        var path = "/Users/wlupus/Downloads/25.png";
+        ClassificationDTO response = cozeImageService.processImageComplianceWithPath(path);
+        log.debug("{}", response);
+    }
 }
